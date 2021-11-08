8b31a1e690cce90dc278819db4d612cf6863ec63
oriest-omg <oriestdjelloh@gmail.com>
2021-11-08 22:07:48 +0000

    4
    finish1

--- Assets/gameOver.cs
+++ Assets/gameOver.cs
@@ -12,13 +12,16 @@ public class gameOver : MonoBehaviour
     public Text txtScoreGame;
     public Text txtScoreGameOver;
 
+    public Text title;
+
     string ScoreGame;
     string scoreGameOver;
 
     public Text bestScore ;
-
     int gameover;
     int best;
+    int win;
+
     
     // Start is called before the first frame update
     void Start()
@@ -33,36 +36,53 @@ public class gameOver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        win = int.Parse(txtScoreGame.text);
+        if(win == 40)
+        {
+            pnl.SetActive(true);
+            title.text = "win";
+            Score();
+        }
     }
     private void OnTriggerEnter2D(Collider2D other) {
         pnl.SetActive(true);
+        Score();
+    }
+    public void loadScenee(){
+        pnl.SetActive(false);
+        SceneManager.LoadScene("Arkanoid");
+    }
+    public void reset()
+    {
+            string path ="bestScore.txt";        
+            string strFile = File.ReadAllText(path);
+            strFile = strFile.Replace(bestScore.text,"0");
+            File.WriteAllText(path, strFile);
+            bestScore.text = "0";
+    }
+
+    private void Score()
+    {
         txtScoreGameOver.text= txtScoreGame.text;
         gameover = int.Parse(txtScoreGameOver.text);
         best = int.Parse(bestScore.text);
-        print("game over :" );
-        print("best : "+best);
-        if( 0 < 1 )
+        if( gameover > best )
         {
             print("ok");
-            string path ="bestScore.txt";
-            //Write some text to the test.txt file
-            StreamWriter writer = new StreamWriter(path, true);
-            writer.WriteLine(txtScoreGameOver.text);
-            writer.Close();
+            string path ="bestScore.txt";        
+            string strFile = File.ReadAllText(path);
+            strFile = strFile.Replace(bestScore.text,txtScoreGameOver.text);
+            File.WriteAllText(path, strFile);
             bestScore.text = txtScoreGameOver.text;
-        }else
+        }
+        else
         {
             print("ok2");
             string path = "bestScore.txt";
             StreamReader reader = new StreamReader(path);
             //Print the text from the file
-            bestScore.text = reader.ReadToEnd().ToString();
+            bestScore.text = reader.ReadLine().ToString();
             reader.Close();
         }
-    }
-    public void loadScenee(){
-        pnl.SetActive(false);
-        SceneManager.LoadScene("Arkanoid");
     }
 }
